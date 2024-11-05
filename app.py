f4aaa5f2e3997091cff02d547ca8ca85ae37d78e
Andy <33376078+498330580@users.noreply.github.com>
2024-11-05 15:42:37 +0800

    添加数据库配置

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 
+# 获取项目目录
+APP_PATH = os.path.dirname(__file__)
+
+
+# 配置
+app.config["DEBUG"] = True
+app.config["SECRET_KEY"] = "123456789abcdefghijklmn"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(APP_PATH, 'data')}/test.db"
+
+
+# 创建数据库连接，管理项目
+db = SQLAlchemy(app)
+
+
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return f'Hello World!  '
 
 
 if __name__ == '__main__':
---
+++ models.py
@@ -0,0 +1,10 @@
+from app import db
+
+
+# 用户
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
