833c5ad05ae64269dfe825a4469b7fae43f1fafa
Andy <33376078+498330580@users.noreply.github.com>
2024-11-11 20:14:42 +0800

    完成爬取数据库api创建和测试

--- biueprint/data.py
+++ biueprint/data.py
@@ -1,8 +1,12 @@
 # 对外提供api对接浏览器插件，储存未进行数据清理的数据至数据库
-from exts import db_sql
+from exts import db_sql, return_json, GetCode
 from models.data import TmpData
 from flask import Blueprint, request, jsonify
-from sqlalchemy import desc, asc, or_    # asc正序，desc倒序
+from sqlalchemy import desc, or_    # asc正序，desc倒序
+
+
+# 创建code类
+code = GetCode()
 
 # v1版api
 data_bp_v1 = Blueprint('data_bp_v1', __name__, url_prefix='/api/v1/data')
@@ -13,7 +17,8 @@ data_bp_v1 = Blueprint('data_bp_v1', __name__, url_prefix='/api/v1/data')
 def v1_list():
     if request.method == 'GET':     # 获取全部数据
         tmp_data = TmpData.query.order_by(desc(TmpData.update_time)).all()
-        return {'code': 200, 'data': tmp_data, 'mess': '获取成功'}, 200
+        results_list = [item.to_json() for item in tmp_data]
+        return {'code': 200, 'data': results_list, 'mess': '获取成功'}, 200
     elif request.method == 'POST' and request.is_json:      # 创建新数据
         data = request.get_json()
         tmp_data = TmpData(**data)
@@ -30,7 +35,7 @@ def v1_one(uid):
     tmp_data = TmpData.query.get(uid)
     if tmp_data:
         if request.method == 'GET':         # 获取单条数据
-            return jsonify({'code': 200, 'data': tmp_data.to_json(), 'mess': '获取成功'}), 200
+            return return_json(tmp_data.to_json())
         elif request.method == 'PUT':       # 更新单条数据
             data = request.json
             tmp_data.name = data['name']
@@ -39,32 +44,30 @@ def v1_one(uid):
             tmp_data.is_sorted = data['is_sorted']
             tmp_data.content = data['content']
             db_sql.session.commit()
-            return jsonify({'code': 200, 'data': tmp_data.to_json(), 'mess': '更新成功'}), 200
+            return return_json(code=code.Update, data=tmp_data.to_json())
         elif request.method == 'DELETE':    # 删除单条数据
             db_sql.session.delete(tmp_data)
             db_sql.session.commit()
-            return jsonify({'code': 204, 'data': tmp_data.to_json(), 'mess': '删除成功'}), 204
+            return return_json(code=code.Del)
         else:
-            return jsonify({'code': 400, 'data': [], 'mess': '方法错误'}), 400
+            return return_json(code=code.Error)
     else:
-        return jsonify({'code': 404, 'data': [], 'mess': '数据不存在'}), 404
+        return return_json(code=code.NotFound)
 
 
 # 核对url是否存在
 @data_bp_v1.route('/search', methods=['GET'])
 def v1_search():
     keyword = request.args.get('keyword')  # 从查询参数中获取关键字
-    if not keyword:
-        return jsonify({'code': 400, 'data': [], 'mess': '没有传入查询关键字 keyword'}), 400
+    search_type = request.args.get('type')  # 搜索类型
 
-    search_type = request.args.get('type')
-    if not search_type:
-        return jsonify({'code': 400, 'data': [], 'mess': '没有传入查询类型关键字 type'}), 400
+    if not keyword and search_type not in ["is_not_sorted", "all_not_crawled"]:
+        return return_json(code=code.Error, mess="没有传入查询关键字 keyword")
 
     if search_type == 'url':    # 搜索url
-        tmp_data = TmpData.query.filter_by(url=keyword).first()
+        tmp_data = TmpData.query.filter(TmpData.url.contains(keyword)).order_by(desc(TmpData.update_time)).all()
         if not tmp_data:
-            return jsonify({'code': 404, 'data': [], 'mess': '数据不存在'}), 404
+            return return_json(code=code.OK, data=[], mess="查询无数据")
     elif search_type == 'name':     # 查找名称
         tmp_data = TmpData.query.filter(TmpData.name.contains(keyword)).all()
     elif search_type == 'content':  # 查找内容
@@ -72,18 +75,24 @@ def v1_search():
     elif search_type == 'is_url':   # 判断url是否存在
         tmp_data = TmpData.query.filter(TmpData.url.contains(keyword)).all()
         if tmp_data:
-            return jsonify({'code': 200, 'data': True, 'mess': '获取成功'}), 200
-        return jsonify({'code': 200, 'data': False, 'mess': '获取成功'}), 200
+            return return_json(data=True, mess="查询存在")
+        return return_json(data=False, mess="查询不存在")
     elif search_type == 'all_not_crawled':
         tmp_data = TmpData.query.filter_by(is_crawled=False).all()
     elif search_type == 'is_not_sorted':
         tmp_data = TmpData.query.filter_by(is_sorted=False).all()
-    else:   # 查找所有匹配的名称和内容
+    else:
         tmp_data = TmpData.query.filter(
             or_(
                 TmpData.name.contains(keyword),
                 TmpData.content.contains(keyword)
             )
         ).all()
-        return jsonify({'code': 200, 'data': tmp_data.to_json(), 'mess': '请传入url参数'}), 200
-    return jsonify({'code': 200, 'data': tmp_data.to_json(), 'mess': '获取成功'}), 200
+        results_list = [item.to_json() for item in tmp_data]
+        if search_type:
+            mess = "传入type参数错误,默认全文搜索"
+        else:
+            mess = "未传入type,默认全文搜索"
+        return return_json(data=results_list, mess=mess)
+    results_list = [item.to_json() for item in tmp_data]
+    return return_json(data=results_list, mess="搜索成功")
--- exts.py
+++ exts.py
@@ -1,7 +1,43 @@
+from time import sleep
+
 from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
 
 
 db_sql = SQLAlchemy()
 
 
+# 状态码
+class GetCode:
+    NotFound = 404  # 不存在
+    OK = 200        # 获取成功
+    Add = 201       # 创建成功
+    Del = 204       # 删除成功
+    Error = 400     # 请求错误
+    Update = 200    # 更新成功
 
+
+_code = GetCode()
+
+
+# 默认返回json
+def return_json(code=_code.OK, data=None, mess=None):
+    if data is None:
+        data = []
+    if mess is None:
+        if code == _code.OK:
+            mess = "获取成功"
+        if code == _code.NotFound:
+            mess = "不存在"
+            data = []
+        if code == _code.Add:
+            mess = "创建成功"
+        if code == _code.Update:
+            mess = "更新成功"
+        if code == _code.Del:
+            mess = "删除成功"
+            data = []
+        if code == _code.Error:
+            mess = "请求错误"
+            data = []
+    return jsonify({"code": code, "data": data, "mess": mess}), code
